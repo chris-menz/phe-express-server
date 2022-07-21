006496c38399ed8504f240f2d51964de2d87ec11
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-07-21 00:14:47 -0400

    cd

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to the PHE Express Dev Server")
 })
 
-app.listen(1001, () => {
-    console.log("Express running on port 1001")
+app.listen(3001, () => {
+    console.log("Express running on port 3001")
 })

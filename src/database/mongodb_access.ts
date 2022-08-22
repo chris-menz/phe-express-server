c903c3dcc2f72ca6fb3f638d015a23e63bf172cc
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2022-08-22 17:23:03 -0400

    mongodb_access.ts stuff

--- src/database/mongodb_access.ts
+++ src/database/mongodb_access.ts
@@ -1,8 +1,8 @@
 import { connectToMongo,  disconnectFromMongo} from "./mongodb";
 import mongoose from "mongoose";
 import express from "express";
-var connection = connectToMongo();
-const candidateScheema = new mongoose.Schema({
+import { CandidateModel } from "../modules/candidate/candidate.model";
+const candidateSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     dateOfBirth: String,
@@ -17,8 +17,30 @@ const candidateScheema = new mongoose.Schema({
     skills: String,
     bio: String
 })
-const employer = undefined;
-function set_up_api(app: express.Application) {
+const employerSchema = new mongoose.Schema({
+    Age: Number,
+    Gender: String,
+    bio: String,
+    location: String,
+    skills: String
+})
+const employerModel = new mongoose.Model('employer', employerSchema);
+const candidateModel = new mongoose.Model('candidate', candidateSchema);
+function err_403(message: String, res: express.Response) {
+    res.status(403);
+    res.send({error: true, reason: message});
+} 
+function err_400(message: String, res: express.Response) {
+    res.status(400);
+    res.send({error: true, reason: message});
+}
+function verify_t (t: String) {
+    // for now
+    return true;
+}
+
+async function set_up_api(app: express.Application) {
+    var connection = await connectToMongo();
     express.json()
     app.route('internal_api/get_db').get( function(req, res, app) {
         res.status(405);
@@ -28,13 +50,77 @@ function set_up_api(app: express.Application) {
         `);
         return 405;
     }).post(
-        function(req,res,app) {
+        async function(req,res,app) {
             // parse the data
-            /* body format:
+            /**  body format:
+                verification: object of format {
+                    t: string
+                }
+                employer_data: null if not an employer, otherwise Object of format {
+                    bio: string
+                    gender: string
+                    age: number
+                    location: string
+                    skills: string
+                    id: number
+                }
+                candidate_data: null if not a candidate, otherwise Object of format {
+
+                }
             */
+            var body = req.body;
+            if (body.verification === undefined) {
+                err_403("you didn't verify", res);
+                return -1;
+            }
+            // body.verification existss
+            if (body.verification.t === undefined) {
+                err_403("the 't' field was not specified, cannot verify", res);
+                return -1;
+            }
+            if (typeof body.verification.t != "string") {
+                err_400("body.verification.t is not a string", res);
+                return -1;
+            }
+            if (!verify_t(body.verification.t)) {
+                err_403("you didn't verify successfully", res);
+                return -1;
+            }
+            
+            if (body.employer_data != null) {
+                var properties = ["bio", "gender", "age", "location", "skills", "id"];
+                for (var i = 0; i<properties.length; i++) {
+                    if (body.employer_data[properties[i]] == undefined) {
+                        err_400("body.employer_data does not contain the property " + properties[i] + "...", res);
+                        return -1;
+                    }
+                    
+                }
+                employerModel.replaceOne({_id: body.employer_data.id}, {
+                    bio: body.employer_data.bio,
+                    age: body.employer_data.age,
+                    gender: body.employer_data.gender,
+                    skills: body.employer_data.skills,
+                    location: body.employer_data.location,
+                    id: body.employer_data.id
+                 });
+                 res.status(200);
+                 res.send({error: false, reason: "", result: null});
+                 return 0;
+            }
+            else if (body.candidate_data != null) {
+
+                res.status(400);
+
+                res.send({error: true, reason: "wrong route"});
+                /*var properties = ["bio", "gender", "age", "location", "skills", "id"];
+                for (var i = 0; i<properties.length; i++) {
+                    if (body.employer_data[properties[i]] == undefined) {
+                        err_400("body.employer_data does not contain the property " + properties[i] + "...", res);
+                        return -1;
+                    }
+                }*/
+            }
         }
     )
-
-
-    
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-"use strict";
 import set_up_app from "database/mongodb_access.ts";
+"use strict";
+
 exports.__esModule = true;
 var express_1 = require("express");
 var app = (0, express_1["default"])();
